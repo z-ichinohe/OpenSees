63711bab04449076d26eef3bf749c0e60dd8a647
Kazuki Ichinohe <z-ichinohe@g.ecc.u-tokyo.ac.jp>
2023-10-09 22:35:39 +0900

    on_backbone declaration before branch shifting

--- SRC/material/uniaxial/TakedaSlip.cpp
+++ SRC/material/uniaxial/TakedaSlip.cpp
@@ -122,9 +122,8 @@ int TakedaSlip::setTrialStrain(double strain, double strainRate)
 
 // Unloading
 // Positive
-    bool on_backbone;
-    on_backbone = (branch == 5 || branch == 6);
-    if ((branch == 3 || branch == 4 || on_backbone) && d_new < d_old) {
+    if ((branch == 3 || branch == 4 || branch == 5 || branch == 6) && d_new < d_old) {
+        bool on_backbone = (branch == 5 || branch == 6);
         branch = 1;
         if (on_backbone) {
             pos_f_global = f_old;
@@ -142,8 +141,8 @@ int TakedaSlip::setTrialStrain(double strain, double strainRate)
         k_tangent = k_unload;
     }
 // Negative
-    on_backbone = (branch == 15 || branch == 16);
-    if ((branch == 13 || branch == 14 || on_backbone) && d_old < d_new) {
+    if ((branch == 13 || branch == 14 || branch == 15 || branch == 16) && d_old < d_new) {
+        bool on_backbone = (branch == 15 || branch == 16);
         branch = 11;
         if (on_backbone) {
             neg_f_global = f_old;
