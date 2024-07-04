31b923291faf99f2813154b73764e05a933e2236
z-ichinohe <z.ichinohe@gmail.com>
2024-07-04 19:55:54 +0900

    "

--- .github/workflows/ops_version_with_githubsha.py
+++ .github/workflows/ops_version_with_githubsha.py
@@ -6,6 +6,8 @@ args: argparse.Namespace = parser.parse_args()
 with open("./SRC/OPS_Globals.h", "r", encoding="utf-8") as fp:
     lines: list[str] = fp.readlines()
     idx: int = [line.startswith("#define OPS_VERSION") for line in lines].index(True)
-    lines[idx] = f"{lines[idx][:-1]} {args.github_sha[0:7]}"
+    line: str = lines[idx]
+    line.replace('"\n', f' {args.github_sha[0:7]}"\n')
+    lines[idx] = line
 with open("./SRC/OPS_Globals.h", "w", encoding="utf-8") as fp:
     fp.writelines(lines)
